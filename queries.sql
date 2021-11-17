a5e17f869b794b7de419e44848d1cd29f05b908d
reags1110 <84357221+reags1110@users.noreply.github.com>
2021-11-17 17:25:11 -0600

    edited queries statement

--- queries.sql
+++ queries.sql
@@ -1,7 +1,9 @@
 -- Part 1: Test it with SQL
 -- list the columns and their data types in the table as a SQL comment.
- SELECT *
- FROM job;
+--id (int, PK),
+--employer (varchar),
+--name(varchar),
+--skills(varchar)
 
 -- Part 2: Test it with SQL
 -- write a query to list the names of the employers in St. Louis City.
